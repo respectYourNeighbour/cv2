048b9bf58fe5066cd10d6b5dd6aa1717cce48426
respectYourNeighbour <philip_luca@yahoo.com>
2016-03-22 17:50:59 +0200

    implemented acl

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,8 @@ angular
 			'satellizer',
 			'toastr',
             'textAngular',
-            'ngSanitize'
+            'ngSanitize',
+			'mm.acl'
 	])
 	.config(function ($stateProvider, $translateProvider, $urlRouterProvider) {
 		function skipIfLoggedIn($q, $auth) {
@@ -48,83 +49,180 @@ angular
 	            templateUrl: 'views/home.html'
 	        })
 	        .state('aboutMe', {
+				resolve: {
+					'acl' : ['$q', 'AclService', function($q, AclService){
+						if(AclService.can('can_about_me')){
+							//Has proper permissions
+							return true;
+						}else{
+							//Does not have permission
+							return $q.reject('Unauthorized');
+						}
+					}]
+				},
 	            templateUrl: 'views/aboutMe.html',
 	            url : '/aboutMe',
 	            controller: 'AboutMeController'
 	        })
 	        .state('articlesList', {
+				resolve: {
+					'acl' : ['$q', 'AclService', function($q, AclService){
+						if(AclService.can('can_article_list')){
+							//Has proper permissions
+							return true;
+						}else{
+							//Does not have permission
+							return $q.reject('Unauthorized');
+						}
+					}]
+				},
 	            templateUrl: 'views/articlesList.html',
 	            url : '/articlesList',
 	            controller: 'ArticleListController'
 	        })
-	        .state('view', {
-	        	url: '/view/:articleId',
-	        	templateUrl: 'views/articleDetail.html',
-	        	controller: 'ArticleDetailController',
-	        	resolve: {
+	        .state('viewArticleDetail', {
+				resolve: {
 	        		resolveGetArticleById: function($stateParams, ArticlesService) {
 	        			return ArticlesService.getArticleById($stateParams.articleId).success(function(data){
 				            console.log('found the article: ', data);
 				        });
-
-	        		}
-	        	}
+	        		},
+					'acl' : ['$q', 'AclService', function($q, AclService){
+						if(AclService.can('can_article_detail')){
+							//Has proper permissions
+							return true;
+						}else{
+							//Does not have permission
+							return $q.reject('Unauthorized');
+						}
+					}]
+	        	},
+	        	url: '/viewArticleDetail/:articleId',
+	        	templateUrl: 'views/articleDetail.html',
+	        	controller: 'ArticleDetailController'
 			})
 	        .state('articleCreate', {
+				resolve: {
+		        	loginRequired: loginRequired,
+					'acl' : ['$q', 'AclService', function($q, AclService){
+						if(AclService.can('can_article_create')){
+							//Has proper permissions
+							return true;
+						}else{
+							//Does not have permission
+							return $q.reject('Unauthorized');
+						}
+					}]
+		        },
 	            templateUrl: 'views/articleCreate.html',
 	            url : '/textEditor',
 	            controller: 'ArticleCreateController',
-	            resolve: {
-		        	loginRequired: loginRequired
-		        }
 	        })
 	        .state('messages', {
+				resolve: {
+					'acl' : ['$q', 'AclService', function($q, AclService){
+						if(AclService.can('can_messages')){
+							//Has proper permissions
+							return true;
+						}else{
+							//Does not have permission
+							return $q.reject('Unauthorized');
+						}
+					}]
+		        },
 		        abstract: true,
 		        url: '/messages',
 		        template: '<ui-view/>'
 		    })
 	        .state('messages.list', {
+				resolve: {
+		        	loginRequired: loginRequired,
+					unreadMessages : function(ContactService) {
+						return ContactService.getUnreadMessages();
+					},
+					'acl' : ['$q', 'AclService', function($q, AclService){
+						if(AclService.can('can_messages')){
+							//Has proper permissions
+							return true;
+						}else{
+							//Does not have permission
+							return $q.reject('Unauthorized');
+						}
+					}]
+		        },
 	            templateUrl: 'views/messages.html',
 	            url : '',
 	            controller: 'MessagesController',
-		        resolve: {
-		        	loginRequired: loginRequired,
-		          	unreadMessages : function(ContactService) {
-		          		return ContactService.getUnreadMessages();
-		          	}
-		        }
 	        })
 	        .state('messages.view', {
+				resolve: {
+					message: function(ContactService,$stateParams) {
+						console.log('$stateParams',$stateParams);
+						return ContactService.getMessage($stateParams.messageId);
+					},
+					'acl' : ['$q', 'AclService', function($q, AclService){
+						if(AclService.can('can_messages')){
+							//Has proper permissions
+							return true;
+						}else{
+							//Does not have permission
+							return $q.reject('Unauthorized');
+						}
+					}]
+	        	},
 	        	url: '/:messageId',
 	        	templateUrl: 'views/messageView.html',
-	        	controller: 'MessageViewController',
-	        	resolve: {
-	          		message: function(ContactService,$stateParams) {
-	          			console.log('$stateParams',$stateParams);
-	          			return ContactService.getMessage($stateParams.messageId);
-	          		}
-	        	}
-	      	})
+	        	controller: 'MessageViewController'
+			})
 	        .state('contact', {
+				resolve: {
+					'acl' : ['$q', 'AclService', function($q, AclService){
+						if(AclService.can('can_contact')){
+							//Has proper permissions
+							return true;
+						}else{
+							//Does not have permission
+							return $q.reject('Unauthorized');
+						}
+					}]
+	        	},
 	            templateUrl: 'views/contact.html',
 	            url : '/contact',
 	            controller: 'ContactController'
 	        })
 	        .state('login', {
+				resolve: {
+					skipIfLoggedIn: skipIfLoggedIn,
+					'acl' : ['$q', 'AclService', function($q, AclService){
+						if(AclService.can('can_login')){
+							//Has proper permissions
+							return true;
+						}else{
+							//Does not have permission
+							return $q.reject('Unauthorized');
+						}
+					}]
+		        },
 	            templateUrl: 'views/login.html',
 	            url : '/login',
-	            controller: 'LoginController',
-		        resolve: {
-		          	skipIfLoggedIn: skipIfLoggedIn
-		        }
+	            controller: 'LoginController'
 	        })
 	        .state('signup', {
+				resolve: {
+					skipIfLoggedIn: skipIfLoggedIn,
+					'acl' : ['$q', 'AclService', function($q, AclService){
+						if(AclService.can('can_signup')){
+							//Has proper permissions
+							return true;
+						}else{
+							//Does not have permission
+							return $q.reject('Unauthorized');
+						}
+					}]
+		        },
 	            templateUrl: 'views/signup.html',
 	            url : '/signup',
-	            controller: 'SignupController',
-		        resolve: {
-		          	skipIfLoggedIn: skipIfLoggedIn
-		        }
+	            controller: 'SignupController'
 	        })
 	        .state('logout', {
 	            template : null,
@@ -132,12 +230,21 @@ angular
 	            controller: 'LogoutController'
 	        })
 	        .state('profile', {
+				resolve: {
+					loginRequired: loginRequired,
+					'acl' : ['$q', 'AclService', function($q, AclService){
+						if(AclService.can('can_profile')){
+							//Has proper permissions
+							return true;
+						}else{
+							//Does not have permission
+							return $q.reject('Unauthorized');
+						}
+					}]
+		        },
 	            templateUrl: 'views/profile.html',
 	            url : '/profile',
-	            controller: 'ProfileController',
-		        resolve: {
-		          	loginRequired: loginRequired
-		        }
+	            controller: 'ProfileController'
 	        })
 	        .state('admin', {
 	            abstract: true,
@@ -171,4 +278,20 @@ angular
             .fallbackLanguage(['en'])
             // remember language
             .useLocalStorage();
-    });
+    })
+	.run(['AclService', '$rootScope', '$location', function(AclService, $rootScope, $location) {
+		var aclData = {
+			guest: ['can_about_me', 'can_article_list', 'can_portofolio', 'can_signup'],
+			member: ['can_about_me', 'can_article_list', 'can_article_detail', 'can_portofolio', 'can_contact', 'can_messages', 'can_login', 'can_signup', 'can_profile'],
+			admin: ['can_about_me', 'can_article_list', 'can_article_detail', 'can_article_create', 'can_portofolio', 'can_contact', 'can_messages', 'can_login', 'can_signup', 'can_profile']
+		};
+		AclService.setAbilities(aclData);
+		AclService.attachRole('admin');
+
+		//If the route change failed due to our "Unauthorized" error, redirect them
+		$rootScope.$on('$routeChangeError', function(current, previous, rejection){
+			if(rejection === 'Unauthorized'){
+				$location.path('/');
+			}
+		});
+	}]);
--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -5,9 +5,11 @@
 (function() {
     'use strict';
 
-    function mainCtrl($scope, $translate, AuthenticationService, $location, $window) {
+    function mainCtrl($scope, $translate, AuthenticationService, $location, $window, AclService) {
         console.log('Main Controller');
         $scope.message = 'hi!';
+
+        $scope.can = AclService.can;
 
         $scope.changeLanguage = function (langKey, event) {
             event.preventDefault();
@@ -23,14 +25,14 @@
             $('#fullscreen').html('<span class="glyphicon glyphicon-resize-small"></span>');
             $('#fullscreen').attr('href', 'http://bootsnipp.com/mouse0270/snippets/PbDb5');
             $('#fullscreen').attr('title', 'Back To Bootsnipp');
-        }    
+        }
         $('#fullscreen').on('click', function(event) {
             event.preventDefault();
             window.parent.location =  $('#fullscreen').attr('href');
         });
         $('#fullscreen').tooltip();
         /* END DEMO OF JS */
-        
+
         $('.navbar-toggler').on('click', function(event) {
             event.preventDefault();
             $(this).closest('.navbar-minimal').toggleClass('open');
@@ -44,6 +46,6 @@
 
     angular
         .module('sampleApp')
-        .controller('MainController',  ['$scope', '$translate', 'AuthenticationService', '$location', '$window', mainCtrl]);
+        .controller('MainController',  ['$scope', '$translate', 'AuthenticationService', '$location', '$window', 'AclService', mainCtrl]);
 
 }());
